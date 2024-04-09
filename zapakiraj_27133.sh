6a35b4d4f6dea454316aeac11ad13e0fa772dc68
DejanRojko <146987239+DejanRojko@users.noreply.github.com>
2024-04-09 17:31:53 +0200

    Update zapakiraj_27133.sh

--- zapakiraj_27133.sh
+++ zapakiraj_27133.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Define variables
+TAG=$(date +"%Y%m%d%H%M%S") # Format: YYYYMMDDHHMMSS
+
 # Login to Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Build the Docker image
-docker build . -f Dockerfile -t "$DOCKER_USERNAME/$REPONAME:latest"
+docker build . -f Dockerfile -t "$DOCKER_USERNAME/$REPONAME:$TAG"
 
 # Push the image to Docker Hub
-docker push "$DOCKER_USERNAME/$REPONAME:latest"
+docker push "$DOCKER_USERNAME/$REPONAME:$TAG"
